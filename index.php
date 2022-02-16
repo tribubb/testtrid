69780cfb8b228939f925613dcbcaef27f92aa84b
tribubb <46801939+tribubb@users.noreply.github.com>
2022-02-16 18:41:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "Distance to Cozumel 15/02/22: 200m<br>";
 echo "Distance to Cozumel 16/02/22: 200m<br>";
   
-echo "Week 1 Distance: 400m<br>";
-echo "Total Distance: 400m";
+echo "<br>Week 1 Distance: 400m<br>";
+echo "<br>Total Distance: 400m";
 ?> 
