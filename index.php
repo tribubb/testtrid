eb3955bce0ba09660ad8aa0d6d4bc9ef10799f88
tribubb <46801939+tribubb@users.noreply.github.com>
2022-02-22 14:46:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
 <html>
-<div>
-<?php
-echo "The swim to Cozumel from the beach at Playa Del Carmen is approximately 17km, being 16.8km at its shortest";
-?> 
-</div>
 <body>
 <?php
 
